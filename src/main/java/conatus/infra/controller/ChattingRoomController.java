ce39a4514ad704ecbea6789f962466d40c212c03
LeeJE20 <silver36c@gmail.com>
2022-08-01 18:02:09 +0900

    REFACTOR: 채팅 레포지터리 리팩터링

--- src/main/java/conatus/infra/controller/ChattingRoomController.java
+++ src/main/java/conatus/infra/controller/ChattingRoomController.java
@@ -3,16 +3,19 @@ package conatus.infra.controller;
 import javax.transaction.Transactional;
 
 import conatus.domain.repository.ChattingRoomRepository;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sample.stomp.service.ChatRoomService;
+
 @RestController
-@RequestMapping(value = "/chattingRooms")
+@RequestMapping(value = "/chat")
+@RequiredArgsConstructor
 @Transactional
 public class ChattingRoomController {
 
-    @Autowired
-    ChattingRoomRepository chattingRoomRepository;
-    // keep
+	private final ChattingRoomService chattingRoomService;
 }
