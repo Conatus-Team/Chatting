d4b93732ca8e42b97070ff31417ba5f98ec10e4e
LeeJE20 <silver36c@gmail.com>
2022-08-01 18:01:21 +0900

    FEAT: 채팅방 입장 메시지

--- src/main/java/conatus/infra/controller/ChattingMessageController.java
+++ src/main/java/conatus/infra/controller/ChattingMessageController.java
@@ -2,6 +2,7 @@ package conatus.infra.controller;
 
 import javax.transaction.Transactional;
 
+import conatus.domain.dto.ChattingMessageDto;
 import conatus.domain.entity.ChattingMessage;
 import conatus.domain.repository.ChattingMessageRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,9 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping(value = "/chattingMessages")
+//@RequestMapping(value = "/chattingMessages")
 @Transactional
 @RequiredArgsConstructor
 public class ChattingMessageController {
@@ -21,10 +23,13 @@ public class ChattingMessageController {
     private final ChattingMessageRepository chattingMessageRepository;
 
 
-    @MessageMapping("/")
-    public void enter(ChattingMessage message) {
+    @MessageMapping("/chat/message")
+    public void enter(ChattingMessageDto message) {
         System.out.println(message);
-
+        if (ChattingMessageDto.MessageType.ENTER.equals(message.getType())) {
+            message.setMessage(message.getNickname()+"님이 입장하였습니다.");
+        }
+        
         // 메시지 보내기
         sendingOperations.convertAndSend("/topic/chat/room/"+message.getGroupId(),message);
 
