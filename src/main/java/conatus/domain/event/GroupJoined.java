c1333a930791266ea445259abc201938d73183fa
KimRiun <kimriun27@gmail.com>
2022-09-07 00:16:13 +0900

    배포용 url 수정

--- src/main/java/conatus/domain/event/GroupJoined.java
+++ src/main/java/conatus/domain/event/GroupJoined.java
@@ -5,14 +5,38 @@ import conatus.infra.AbstractEvent;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class GroupJoined extends AbstractEvent {
 
-    private Long id;
-    private Long groupId;
-    private Long userId;
-    private String category="";
-    private String groupName="";
-    // keep
+//    private Long id;
+//    private Long groupId;
+//    private Long userId;
+//    private String category="";
+//    private String groupName="";
+
+    private Member member;
+
+    public GroupJoined(Member aggregate) {
+        super(aggregate);
+        member = aggregate;
+    }
+
+    public GroupJoined() {
+        super();
+    }
+
+    @Data
+    public class Member {
+        LocalDateTime createdTime;
+        LocalDateTime updatedTime;
+        Long id;
+        Boolean isDeleted = Boolean.FALSE;
+        Long userId;
+        String nickname;
+        Boolean isLeader = Boolean.FALSE;
+        Long groupId;
+    }
 
 }
